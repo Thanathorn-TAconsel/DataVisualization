2bd2598466c1ba09f40b8aa7a954b2b70c71b868
BuildTools <unconfigured@null.spigotmc.org>
2022-01-26 14:55:12 +0700

    test push 1

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
         Scanner scan = new Scanner(fin);
         while (scan.hasNextLine()) {
             System.out.println(scan.nextLine());
+            System.out.println("Helloworld");
         }
     }
     public static void main(String[] args) throws FileNotFoundException {
